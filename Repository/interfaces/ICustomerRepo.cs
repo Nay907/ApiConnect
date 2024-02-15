f2750bd805b0f6d860736f7cdb1f850466ad8ef6
Nay907 <nayanika.mishra02@gmail.com>
2024-02-15 10:38:47 +0530

    main
    done

---
+++ Repository/interfaces/ICustomerRepo.cs
@@ -0,0 +1,13 @@
+﻿using api.Models;
+
+namespace api.Repository.interfaces
+{
+    public interface ICustomerRepo
+    {
+        List<Customer> GetCustomers();
+        Customer GetCustomerById(string id);
+        Customer CreateCustomer(Customer customer);
+        Customer UpdateCustomer(Customer customer);
+        bool DeleteCustomer(string id);
+    }
+}
